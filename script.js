d35d07b69d69d44c5bd6644bbd769e546a83a841
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-03-04 15:29:14 -0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const billAmount = document.getElementById('bill');
+const peopleNumber = document.getElementById('people-number');
+
+const allButtons = document.querySelectorAll('.select__porcentages__btn');
+const btn5 = document.querySelector('btn-5');
+const btn10 = document.querySelector('btn-10');
+const btn15 = document.querySelector('btn-15');
+const btn25 = document.querySelector('btn-25');
+const btn50 = document.querySelector('btn-50');
+const btnCustom = document.getElementById('custom');
+
+const resultTipAmount = document.getElementById('tip-amount');
+const resultTotal = document.getElementById('total');
