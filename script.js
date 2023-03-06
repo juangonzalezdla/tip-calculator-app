354d8996f7fa66d0981585fd1cd3e954acfa01f3
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-03-06 15:24:51 -0500

    Add reset functions

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const billAmount = document.getElementById('bill');
-const peopleNumber = document.getElementById('people-number');
+const InputBill = document.getElementById('bill');
+const InputPeople = document.getElementById('people-number');
+const showError = document.querySelector('label span');
 
-const allButtons = document.querySelectorAll('.select__porcentages__btn');
-const btn5 = document.querySelector('btn-5');
-const btn10 = document.querySelector('btn-10');
-const btn15 = document.querySelector('btn-15');
-const btn25 = document.querySelector('btn-25');
-const btn50 = document.querySelector('btn-50');
-const btnCustom = document.getElementById('custom');
+const btn5 = document.querySelector('.btn-5');
+const btn10 = document.querySelector('.btn-10');
+const btn15 = document.querySelector('.btn-15');
+const btn25 = document.querySelector('.btn-25');
+const btn50 = document.querySelector('.btn-50');
+const inputCustom = document.getElementById('custom');
 
 const resultTipAmount = document.getElementById('tip-amount');
 const resultTotal = document.getElementById('total');
+const btnReset = document.querySelector('.btn__reset');
+
+let billAmount, peopleNumber, CustomPercent, tipPerson, tipTotal, totalPerson;
+
+/* Reset Functions */
+const resetInputValues = () => {
+  InputBill.value = '';
+  InputPeople.value = '';
+  InputPeople.classList.remove('empty');
+  inputCustom.value = '';
+  showError.classList.remove('empty');
+}
+const resetPercentageBtns = () => {
+  percentageBtns.forEach((btn) => {
+    btn.classList.remove('selected');
+  });
+}
+const resetResultValues = () => {
+  resultTipAmount.value = '$0.00';
+  resultTotal.value = '$0.00';
+}
